c44b9a5054e9f57022e45fda26f7e71a857e1ffb
Levi Ramsey <leviable@gmail.com>
2015-04-20 10:48:15 -0400

    DrawerList arrays are effectively 1-based...

--- therappy/src/main/java/com/jjclade/therappy/MainUI.java
+++ therappy/src/main/java/com/jjclade/therappy/MainUI.java
@@ -15,7 +15,7 @@ public class MainUI extends Activity
 		tx.replace(R.id.content_frame, fragment);
 
 		if (enableBack) {
-			tx.addToBackStack(null);4
+			tx.addToBackStack(null);
 		}
 
 		tx.commit();
@@ -45,16 +45,16 @@ public class MainUI extends Activity
 		Fragment fragment;
 		
 		switch (position) {
-			case 0:
+			case 1;
 				fragment=mainMenu;
 				break;
-			case 1:
-				return;
 			case 2:
 				return;
 			case 3:
 				return;
 			case 4:
+				return;
+			case 5:
 				return;
 			default:
 				return;
@@ -63,7 +63,7 @@ public class MainUI extends Activity
 		switchContent(fragment, true);
 
 		drawerList.setItemChecked(position, true);
-		if (position > 0) {
+		if (position > 1) {
 			getActionBar().setTitle(drawerMenuItems[position]);
 		}
 		drawerLayout.closeDrawer(drawerList);
