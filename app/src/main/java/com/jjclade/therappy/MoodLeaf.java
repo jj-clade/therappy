131f85d71445c1b0f0216007b717ffb4496492d7
jakethrocky <jthrockm@umass.edu>
2015-05-05 18:59:58 -0400

    added LogEntry class and StringLeaf and MoodLeaf support classes

---
+++ app/src/main/java/com/jjclade/therappy/MoodLeaf.java
@@ -0,0 +1,14 @@
+package com.jjclade.therappy;
+
+/**
+ * Created by jacob on 5/5/15.
+ */
+public class MoodLeaf extends StringLeaf {
+    public double intensity;
+    public MoodLeaf(String s){
+        super(s);
+        intensity = 0;
+    }
+
+
+}
