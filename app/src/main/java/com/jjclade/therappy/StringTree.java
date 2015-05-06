2327ae2273ea0357af2ef18e6368d52bcbd70cc9
jakethrocky <jthrockm@umass.edu>
2015-05-06 11:12:38 -0400

    fixed a minor error in stringtree

--- app/src/main/java/com/jjclade/therappy/StringTree.java
+++ app/src/main/java/com/jjclade/therappy/StringTree.java
@@ -14,7 +14,12 @@ public class StringTree {
         kids = new ArrayList<StringTree>();
     }
 
-    public void add(String s){
+    public void add(StringTree s){
         kids.add(s);
     }
 }
+
+
+
+
+
