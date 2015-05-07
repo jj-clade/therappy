d93ed66a3edfb3a4cf6a5c7e2a249c5ccf4e850f
DavidDeAngelo <ddeangel@umass.edu>
2015-05-07 14:54:20 -0400

    Comit

--- app/src/main/java/com/jjclade/therappy/GetHelp.java
+++ app/src/main/java/com/jjclade/therappy/GetHelp.java
@@ -1,109 +1,196 @@
 package com.jjclade.therappy;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link GetHelp.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link GetHelp#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class GetHelp extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GetHelp.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GetHelp newInstance(String param1, String param2) {
-        GetHelp fragment = new GetHelp();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public GetHelp() {
-        // Required empty public constructor
-    }
-
+public class GetHelp extends Main {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        setTitle("Get Help");
+
+        if (savedInstanceState == null) {
+            switchFragmentTo(1);
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_help, container, false);
+    private void switchFragmentTo(int pageNumber) {
+        FragmentTransaction tx=getFragmentManager().beginTransaction();
+
+        switch (pageNumber) {
+            case 1:
+                tx.replace(R.id.content_frame, new Page1());
+                break;
+            case 2:
+                tx.replace(R.id.content_frame, new Page2());
+                break;
+            case 3:
+                tx.replace(R.id.content_frame, new Page3());
+                break;
+            case 4:
+                tx.replace(R.id.content_frame, new Page4());
+                break;
+            case 5:
+                tx.replace(R.id.content_frame, new Page5());
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), "There's no page for that", Toast.LENGTH_SHORT);
+                return;
+        }
+
+        tx.commit();
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private class Page1 extends Fragment {
+        public Page1() {
+            // empty as required for Fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView=inflater.inflate(R.layout.fragment_get_help, container, false);
+
+            ImageButton button=(ImageButton)(rootView.findViewById(R.id.next_button));
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(2);
+                }
+            });
+            Button add=(Button)(rootView.findViewById(R.id.add));
+            add.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(3);
+                }
+            });
+
+
+
+//            Button button1=(Button)(rootView.findViewById(R.id.next_button));
+//            button.setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View v) {
+//                    // TODO: check that a mood is entered
+//                    switchFragmentTo(2);
+//                }
+//            });
+
+            return rootView;
+        }
+
+        private LogMoods activity;
+    }
+
+    private class Page2 extends Fragment {
+        public Page2() {
+            // empty as required for Fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView=inflater.inflate(R.layout.fragment_get_help2, container, false);
+
+            Button button=(Button)(rootView.findViewById(R.id.next_button));
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(1);
+                }
+            });
+
+            return rootView;
         }
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        /*try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
+
+    private class Page5 extends Fragment {
+        public Page5() {
+            // empty as required for Fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView=inflater.inflate(R.layout.fragment_get_helpmom, container, false);
+
+            Button button=(Button)(rootView.findViewById(R.id.next_button));
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(4);
+                }
+            });
+
+            return rootView;
+        }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    private class Page3 extends Fragment {
+        public Page3() {
+            // empty as required for Fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView=inflater.inflate(R.layout.fragment_get_help3, container, false);
+
+            Button button=(Button)(rootView.findViewById(R.id.next_button));
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(4);
+                }
+            });
+
+            return rootView;
+        }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    private class Page4 extends Fragment {
+        public Page4() {
+            // empty as required for Fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView=inflater.inflate(R.layout.fragment_get_helpother, container, false);
+
+            ImageButton button=(ImageButton)(rootView.findViewById(R.id.next_button));
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(2);
+                }
+            });
+
+            ImageButton mom=(ImageButton)(rootView.findViewById(R.id.imageButton5));
+            mom.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(5);
+                }
+            });
+
+            Button add=(Button)(rootView.findViewById(R.id.addnew));
+            add.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO: check that a mood is entered
+                    switchFragmentTo(3);
+                }
+            });
+
+
+            return rootView;
+        }
     }
+
 
 }
